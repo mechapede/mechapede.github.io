d6e8979d670ed75559d98c7a63c0761bfbe1d17a
Mechapede <26446679+mechapede@users.noreply.github.com>
2020-06-28 18:09:55 -0700

    Tweak webdemo constants

---
+++ assets/webalg/alg.js
@@ -0,0 +1,7 @@
+// Wrapper for calling c code and buttons
+
+document.getElementById("calc_result").onclick = function(){
+  console.log(document.getElementById("input_seq").value);
+  document.getElementById("result_seq").value = "world of pain";
+};
+
